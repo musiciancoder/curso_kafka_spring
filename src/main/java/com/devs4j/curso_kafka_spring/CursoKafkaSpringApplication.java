f9b082f9578ee4d0e0ca7f6d02eaa0695957b7bc
musiciancoder <andresguitarra1980@gmail.com>
2023-02-09 11:32:35 -0300

    utilizando un @kafka listener

--- src/main/java/com/devs4j/curso_kafka_spring/CursoKafkaSpringApplication.java
+++ src/main/java/com/devs4j/curso_kafka_spring/CursoKafkaSpringApplication.java
@@ -1,12 +1,23 @@
 package com.devs4j.curso_kafka_spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class CursoKafkaSpringApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(CursoKafkaSpringApplication.class);
+
+	@KafkaListener(topics= "devs4j-topic", groupId = "devs4j-group")
+     public void listen(String message){ //este metodo va a recibir mensajes de kafka
+		log.info("Message received {}", message);
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(CursoKafkaSpringApplication.class, args);
 	}
 
