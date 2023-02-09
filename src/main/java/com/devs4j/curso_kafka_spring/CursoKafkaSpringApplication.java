15bfd63749268bb0c1ee3fb77f32aad15b6dc6ac
musiciancoder <andresguitarra1980@gmail.com>
2023-02-09 19:13:08 -0300

    Producer síncrono

--- src/main/java/com/devs4j/curso_kafka_spring/CursoKafkaSpringApplication.java
+++ src/main/java/com/devs4j/curso_kafka_spring/CursoKafkaSpringApplication.java
@@ -13,6 +13,8 @@ import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class CursoKafkaSpringApplication implements CommandLineRunner {
 
@@ -37,8 +39,15 @@ public class CursoKafkaSpringApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		//kafkaTemplate.send("devs4j-topic","Sample message"); //al principio solo corrió esta linea para probar
+
+		//código sincrono
+		kafkaTemplate.send("devs4j-topic","Sample message").get(100, TimeUnit.MILLISECONDS);
+
+		/*
+
+		//Código asincrono
 		ListenableFuture<SendResult<String,String>>future = (ListenableFuture<SendResult<String, String>>) kafkaTemplate.send("devs4j-topic","Sample message");
-	future.addCallback(new KafkaSendCallback<String, String>(){
+	    future.addCallback(new KafkaSendCallback<String, String>(){
 
 		@Override
 		public void onSuccess(SendResult<String, String> result) {
@@ -49,6 +58,6 @@ public class CursoKafkaSpringApplication implements CommandLineRunner {
 		public void onFailure(KafkaProducerException e) {
               log.error("Error",e);
 		}
-	});
+	});*/
 	}
 }
