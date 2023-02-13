218ecc697116e7d1e809c14205b54dc296738225
musiciancoder <andresguitarra1980@gmail.com>
2023-02-13 14:38:31 -0300

    Acceso a métricas

--- src/main/java/com/devs4j/curso_kafka_spring/CursoKafkaSpringApplication.java
+++ src/main/java/com/devs4j/curso_kafka_spring/CursoKafkaSpringApplication.java
@@ -1,5 +1,6 @@
 package com.devs4j.curso_kafka_spring;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,26 +21,27 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class CursoKafkaSpringApplication implements CommandLineRunner {
+//public class CursoKafkaSpringApplication implements CommandLineRunner { //lo quitó (junto con el metodo run) en la seccion Acceso a métricas y lo reemplazó linea de abajo
+public class CursoKafkaSpringApplication {
 
-	@Autowired
-	private KafkaTemplate<String, String> kafkaTemplate; //guardado con @Bean en clase KafkaConfiguration
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate; //guardado con @Bean en clase KafkaConfiguration
+
+    @Autowired
+    private MeterRegistry meterRegistry; //lo agrego en seccion Acceso a métricas
 
 	/*
 	@Autowired
 	private KafkaListenerEndpointRegistry registry; //Lo agregó en seccion "Pausando y reanudando consumo de mensajes"
+
 */
 
-	@Scheduled(fixedDelay = 1000, initialDelay = 500) //cada cierto tiempo se ejecuta esto. Se usa en combinacion con @EnableScheduling
-	public void print(){
-		log.info("Devs4j rocks!");
-	}
 
-	private static final Logger log = LoggerFactory.getLogger(CursoKafkaSpringApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(CursoKafkaSpringApplication.class);
 
-	//con @KafkaListener podemos recibir mensajes enviados desde consola comandos del producer
+    //con @KafkaListener podemos recibir mensajes enviados desde consola comandos del producer
 
-	//Sin Batch
+    //Sin Batch
 	/*
 	@KafkaListener(topics= "devs4j-topic",groupId = "devs4j-group")
 	public void listen(String message){
@@ -49,32 +51,55 @@ public class CursoKafkaSpringApplication implements CommandLineRunner {
 	 */
 
 
-	//Con Batch
-	@KafkaListener(
-			       id="devs4jId",
-				   autoStartup = "false", //por defecto esta en true. Lo agregó en seccion "Pausando y reanudando consumo de mensajes"
-			       topics= "devs4j-topic",
-			       containerFactory = "listenerContainerFactory", //esto es para los batch. debemos escribir el nombre del bean que le dimos en KafkaConfiguration
-			       groupId = "devs4j-group",
-	properties = {"max.poll.interval.ms:4000",//los batch cada 4 segundos
-	"max.poll.records:10"})//los batch de 10 en 10
-     public void listen //este metodo va a recibir mensajes de kafka
-		(List<ConsumerRecord<String,String>> messages){   //al principio recibia una lista de string como argumento...pero en seccion Accediendo a la información completa del mensaje (para mostrar particion, offset, key y value) cambio a ConsumerRecord
-		log.info("Start reading messages");
+    //Con Batch
+    @KafkaListener(
+            id = "devs4jId",
+            autoStartup = "true", //por defecto esta en true. Lo agregó en seccion "Pausando y reanudando consumo de mensajes". En seccion Acceso a métricas lo dejo en true nuevamente
+            topics = "devs4j-topic",
+            containerFactory = "listenerContainerFactory", //esto es para los batch. debemos escribir el nombre del bean que le dimos en KafkaConfiguration
+            groupId = "devs4j-group",
+            properties = {"max.poll.interval.ms:4000",//los batch cada 4 segundos
+                    "max.poll.records:50"})//los batch de 10 en 10 (despues lo cambió a 50)
+    public void listen //este metodo va a recibir mensajes de kafka
+    (List<ConsumerRecord<String, String>> messages) {   //al principio recibia una lista de string como argumento...pero en seccion Accediendo a la información completa del mensaje (para mostrar particion, offset, key y value) cambio a ConsumerRecord
+        log.info("Messages received {}", messages.size());
+        //	log.info("Start reading messages");
+		/*
+		//lo comentó en seccion  Acceso a métricas
 		for (ConsumerRecord<String,String>msg:messages) {
 			log.info("Partition= {}, Offset={}, Key={}, Value= {}", msg.partition(), msg.offset(), msg.key(), msg.value());
 		}
-		log.info("Batch complete");
-	}
 
-	public static void main(String[] args) {
+		 **/
+        //	log.info("Batch complete");
+    }
 
-		SpringApplication.run(CursoKafkaSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(CursoKafkaSpringApplication.class, args);
+    }
+
+    @Scheduled(fixedDelay = 2000, initialDelay = 100)
+    //cada cierto tiempo se ejecuta esto. Se usa en combinacion con @EnableScheduling
+    public void sendKafkaMessages() {
+        //	log.info("Devs4j rocks!");
+        for (int i = 0; i < 100; i++) {
+            kafkaTemplate.send("devs4j-topic", String.valueOf(i), String.format("Sample message %d", i));
+        }
+    }
+
+    //Acceso a métricas
+    @Scheduled(fixedDelay = 2000, initialDelay = 500)
+    public void printMetrics() {
+        double count = meterRegistry.get("kafka.producer.record.send.total").functionCounter().count();
+        log.info("Count {} ", count); //cuenta de cuantos mensajes se han entregado
+
+    }
 
 
+    //ver mensajes que se estan enviando en los logger de spring
 
-	//ver mensajes que se estan enviando en los logger de spring
+	/*
 	@Override
 	public void run(String... args) throws Exception {
 		//kafkaTemplate.send("devs4j-topic","Sample message"); //al principio solo corrió esta linea para probar
@@ -99,10 +124,9 @@ public class CursoKafkaSpringApplication implements CommandLineRunner {
 		}
 	});*/
 
-		//Codigo con batch (ni sincrono ni asincrono)
+    //Codigo con batch (ni sincrono ni asincrono)
 		/*
 		for (int i = 0; i < 100; i++) {
-			kafkaTemplate.send("devs4j-topic",String.valueOf(i),String.format("Sample message %d", i));
 		}
 		//código agregado en sección Pausando y reanudando el consumo de mensajes
 		/*
@@ -115,5 +139,8 @@ public class CursoKafkaSpringApplication implements CommandLineRunner {
 		Thread.sleep(5000);
 		registry.getListenerContainer("devs4jId").stop(); //parar automaticamente consumer luego de 5 sefundos
 */
-	}
+
 }
+
+
+
